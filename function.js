855d7e0d51239fe917db84fcb3820285eaf57a67
Ashish Rana <ashish.rana44@gmail.com>
2023-04-12 01:02:24 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,2 +1,13 @@
 const profilePictureInput = document.getElementById('profile-picture-input');
 profilePictureInput.addEventListener('change', handleProfilePictureUpload);
+
+function handleProfilePictureUpload(event) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+  reader.addEventListener('load', () => {
+    const profilePicturePreview = document.getElementById('profile-picture-preview');
+    profilePicturePreview.src = reader.result;
+  });
+  reader.readAsDataURL(file);
+}
+
